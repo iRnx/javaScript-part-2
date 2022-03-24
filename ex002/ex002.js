e240a4fe6b622b6690bcbdf456cbd865c6fda3fc
Renan <86449025+iRnx@users.noreply.github.com>
2022-03-23 21:31:45 -0300

    Declaração de função e Expressão de função

---
+++ ex002/ex002.js
@@ -0,0 +1,9 @@
+// Declaração de função
+
+function minhaFuncao(param){
+    // Bloco de código
+}
+
+// Expressão de função
+const soma = function(num1, num2) {return num1 + num2}
+console.log(soma(5, 5))
