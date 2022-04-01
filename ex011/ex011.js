0ff5ba219825a16c32e7e69814a887c9a2a5bee6
Renan <86449025+iRnx@users.noreply.github.com>
2022-04-01 14:26:41 -0300

    guardando 2 array, dentro de outro array

--- ex011/ex011.js
+++ ex011/ex011.js
@@ -1,1 +1,6 @@
+const alunos = ['Itachi', 'Minato', 'Pain', 'obito']
+const medias = [10, 7, 9, 6]
 
+const listaDeNotasEAlunos = [alunos, medias]
+
+console.log(`${listaDeNotasEAlunos[0][1]}, Sua média é: ${listaDeNotasEAlunos[1][0]}`)
