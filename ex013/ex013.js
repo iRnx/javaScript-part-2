491865b72ab2e6764c2bd8b325c39ed0260dbb3c
Renan <86449025+iRnx@users.noreply.github.com>
2022-04-03 20:28:07 -0300

    Usando for com arrays

--- ex013/ex013.js
+++ ex013/ex013.js
@@ -1,1 +1,11 @@
+const numeros = [100, 200, 300, 400, 500, 600]
 
+for(i = 0; i < numeros.length; i++){
+    console.log(i, numeros[i])
+}
+
+console.log('---------------------------------------------------')
+
+for(pos in numeros){
+    console.log(`${Number(pos) + 1} ${numeros[pos]}`)
+}
