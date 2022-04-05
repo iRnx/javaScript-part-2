6424305abb7a3a481870e560b96456754f8576fa
Renan <86449025+iRnx@users.noreply.github.com>
2022-04-05 15:58:07 -0300

    Calculando Soma das notas e a média

--- ex015/ex015.js
+++ ex015/ex015.js
@@ -1,1 +1,9 @@
+var notas = [1, 2, 3, 4, 5]
+let soma = 0
+for(c in notas){
+    soma += notas[c]
+}
 
+let media = soma / notas.length
+
+console.log(`A soma das notas é: ${soma}, e a Média é: ${media}`)
