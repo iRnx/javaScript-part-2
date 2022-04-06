fa49128ed95386ee6c72317f152640e09157fe69
Renan <86449025+iRnx@users.noreply.github.com>
2022-04-06 13:32:13 -0300

    callback, forEach

--- ex016/ex016.js
+++ ex016/ex016.js
@@ -1,1 +1,17 @@
+const notas = [1, 2, 3, 4, 5]
+let soma = 0
 
+// callback
+notas.forEach(nota => {
+    soma += nota
+})
+
+// Segunda forma de fazer um forEach
+
+notas.forEach(function(nota){
+    soma += nota
+})
+
+let media = soma / notas.length
+
+console.log(media)
