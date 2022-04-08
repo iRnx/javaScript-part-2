cc2a6efd40188bed9bab453a4f003560c29b2930
Renan <86449025+iRnx@users.noreply.github.com>
2022-04-07 22:07:39 -0300

    Funções com forEach

--- ex017/ex017.js
+++ ex017/ex017.js
@@ -1,1 +1,27 @@
+const nomes = ['Pain', 'Itachi', 'Kisame', 'Konan']
 
+// Essa forma a gnt cria uma arrow function e coloca ela dentro do forEach
+const teste = nome => {
+    console.log(nome)
+}
+nomes.forEach(teste)
+console.log('----------------------------------------')
+
+// Essa forma a gnt cria uma função normal e coloca ela dentro do forEach
+nomes.forEach(imprime)
+
+function imprime(nome){
+    console.log(nome)
+}
+console.log('----------------------------------------')
+
+// Essa forma a gnt ja cria uma (arrow function) no proprio forEach
+nomes.forEach(nome => {
+    console.log(nome)
+})
+console.log('----------------------------------------')
+
+// Essa forma a gnt cria uma (Função Normal sem nome apenas com parametro) dentro do forEach
+nomes.forEach(function(nome){
+    console.log(nome)
+})
