e97e416c7117197f68518265a7acfa1e268816ef
Renan <86449025+iRnx@users.noreply.github.com>
2022-04-15 20:18:56 -0300

    criando um objeto

--- ex022/ex022.js
+++ ex022/ex022.js
@@ -1,1 +1,9 @@
+// Um objeto é formado por uma cadeia de chave e valor
 
+// Como criar um objeto ↓
+const cliente = {
+    nome: "Renan",
+    idade: 23,
+    cpf: "322440000",
+    email: "ggdomangue@gmail.com"
+}
