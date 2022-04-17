0bc72f7c7af17c95f2a0c226e58ec3282379ca4a
Renan <86449025+iRnx@users.noreply.github.com>
2022-04-17 00:16:29 -0300

    acessar um objeto com notação de ponto e substring

--- ex023/ex023.js
+++ ex023/ex023.js
@@ -1,1 +1,14 @@
+const cliente = {
+    nome: "Renan",
+    idade: 23,
+    cpf: "32244896398",
+    email: "ggdomangue@gmail.com"
+}
+
+// Forma de acessar um objeto com notação de ponto
+console.log(`Meu nome é ${cliente.nome}, minha idade é ${cliente.idade}, meu cpf é ${cliente.cpf} e meu email é ${cliente.email}`)
+
+// Para pegar os 3 primeiros digitos de uma string
+console.log(cliente.cpf.substring(0, 3))
+
 
