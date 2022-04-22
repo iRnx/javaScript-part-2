197dd4ec3e452787a0f1fc6fd356099d7d77098e
Renan <86449025+iRnx@users.noreply.github.com>
2022-04-22 19:25:47 -0300

    forma de acessar um objeto com notação de colchete, e forEach

--- ex024/ex024.js
+++ ex024/ex024.js
@@ -1,1 +1,17 @@
+const cliente = {
+    nome: "Renan",
+    idade: 23,
+    cpf: "32244896398",
+    email: "ggdomangue@gmail.com"
+}
+
+// forma de acessar um objeto com notação de colchete
+const chaves = ['nome', 'idade', 'cpf', 'email']
+console.log(cliente[chaves[2]])
+console.log(cliente['email'])
+
+// Fazendo um forEach com Arrow Function para percorrer todo objeto
+chaves.forEach(info => console.log(cliente[info]))
+
+
 
